49749fa1971cf6d73a1077fb5970e877f77342ef
drydin-reynolds <drydin.reynolds@outlook.com>
2025-02-14 14:46:48 -0600

    Configure Sepolia

--- eth-contracts/hardhat.config.ts
+++ eth-contracts/hardhat.config.ts
@@ -1,8 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.DEPLOYER_KEY!],
+    },
+  },
 };
 
 export default config;
