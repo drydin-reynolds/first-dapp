af5477866bfb58d3e44c2cf94e0b9a72d2622205
drydin-reynolds <drydin.reynolds@outlook.com>
2025-02-14 15:17:15 -0600

    Fix config

--- eth-contracts/hardhat.config.ts
+++ eth-contracts/hardhat.config.ts
@@ -8,7 +8,7 @@ const config: HardhatUserConfig = {
   networks: {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
-      accounts: [process.env.DEPLOYER_KEY!],
+      accounts: process.env.DEPLOYER_KEY ? [process.env.DEPLOYER_KEY] : [],
     },
   },
 };
